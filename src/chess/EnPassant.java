1aedabda2ebdf196c28c3e6a45cfb3523fcf81eb
marc726 <marcrizzolo726@gmail.com>
2023-10-14 13:35:39 -0400

    enpassant info

--- src/chess/EnPassant.java
+++ src/chess/EnPassant.java
@@ -2,6 +2,15 @@ package chess;
 
 import chess.ReturnPiece.PieceType;
 
+
+    /**
+     * Checks if pawn can capture via en passant.
+     *
+     * @param currentPawn pawn attempting the en passant move.
+     * @param targetPosition  target position for the pawn move.
+     * @return ret true if en passant is possible, else false.
+     */
+
 public class EnPassant {
     
     public static boolean canEnPassant(ReturnPiece pawn, String end) {
